436afe81c34372da2dc4343356d9bee783e4845d
Enzo Piriou <enzopiriou@gmail.com>
2022-05-18 00:42:17 +0200

    @10

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -2,6 +2,7 @@
 	export async function load({ fetch }) {
 		const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 		const guides = await res.json();
+		console.log(guides);
 
 		if (res.ok) {
 			return {
